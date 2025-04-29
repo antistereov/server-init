e18c84dc8e100221645599d44934c514a8b0a317
antistereov <andre.antimonov@stereov.io>
2025-04-29 11:48:12 +0200

    fix(control_plane): add shebang and export variables for clarity

--- kubernetes/control_plane.sh
+++ kubernetes/control_plane.sh
@@ -1,6 +1,8 @@
-IPADDR=$(curl -4 ifconfig.me && echo "")
-NODENAME=$(hostname -s)
-POD_CIDR="192.168.0.0/16"
+#!/bin/bash
+
+export IPADDR=$(curl -4 ifconfig.me && echo "")
+export NODENAME=$(hostname -s)
+export POD_CIDR="192.168.0.0/16"
 
 sudo kubeadm init --control-plane-endpoint=$IPADDR  --apiserver-cert-extra-sans=$IPADDR  --pod-network-cidr=$POD_CIDR --node-name $NODENAME
 
